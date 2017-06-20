9eaa3ba54b2bc7d39e60baafead444008b66cdc1
dallinxie <dallinxie@163.com>
2017-06-20 20:07:24 +0800

    3-4 修改模型 job， 增加对薪资字段的校验

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,7 @@
 class Job < ApplicationRecord
   validates :title, presence: true,    #标题不能为空
                 length: {minimum: 4}   #长度必须>=4
+  validates :wage_upper_bound, presence: true   # 最高薪资不能为空
+  validates :wage_lower_bound, presence: true   # 最低薪资不能为空
+  validates :wage_lower_bound, numericality: {greater_than: 3}  # 最低薪资要大于零
 end
